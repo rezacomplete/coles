dc56e03216613bdfd9e24357aa2b65be5133d574
Reza Ahmadi <rezacomplete@gmail.com>
2026-03-13 19:06:22 +1100

    Init project

---
+++ src/main/java/com/example/coles/ColesApplication.java
@@ -0,0 +1,13 @@
+package com.example.coles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coles/ColesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
